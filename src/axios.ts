47073e9512de55f68de20bba9bc1fee223d286aa
yanxinchen1990 <yxc900920@163.com>
2019-08-13 15:40:59 +0800

    api扩展

--- src/axios.ts
+++ src/axios.ts
@@ -1,39 +1,16 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types';
-import xhr from './xhr';
-import { buildURL } from './helpers/url';
-import { transformRequest, transformResponse } from './helpers/data';
-import { processHeaders } from './helpers/headers';
+import Axios from './core/Axios';
+import { AxiosInstance } from './types';
+import { extend } from './helpers/util';
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config);
-  return xhr(config).then(response => transformResponseData(response));
+function createInstance(): AxiosInstance {
+  const context = new Axios();
+  const instance = Axios.prototype.request.bind(context);
+
+  extend(instance, context);
+
+  return instance as AxiosInstance;
 }
 
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config);
-  config.headers = transformHeaders(config);
-  config.data = transformRequestData(config);
-}
-
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config;
-  return buildURL(url, params);
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  const { data } = config;
-  return transformRequest(data);
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config;
-  return processHeaders(headers, data);
-}
-
-function transformResponseData(response: AxiosResponse): AxiosResponse {
-  const { data } = response;
-  response.data = transformResponse(data);
-  return response;
-}
+const axios = createInstance();
 
 export default axios;
--- src/types/index.ts
+++ src/types/index.ts
@@ -15,7 +15,7 @@ export type Method =
   | 'PATCH';
 
 export interface AxiosRequestConfig {
-  url: string; // 请求URL地址
+  url?: string; // 请求URL地址
   method?: Method; // 请求方式
   data?: any; // 请求体
   params?: any; // 请求参数
@@ -41,4 +41,26 @@ export interface AxiosError extends Error {
   request?: any;
   response?: AxiosResponse;
   isAxiosError: boolean;
+}
+
+export interface Axios {
+  request(config: AxiosRequestConfig): AxiosPromise;
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise;
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise;
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise;
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise;
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise;
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise;
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise;
+}
+
+export interface AxiosInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise;
 }
