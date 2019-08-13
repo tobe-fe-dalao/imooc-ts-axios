81834d12773c19165ab58a2ec4979e8d32ac69f0
yanxinchen1990 <yxc900920@163.com>
2019-08-13 17:40:13 +0800

    返回数据类型

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -44,3 +44,32 @@ axios('/extend/post', {
     msg: 'hi'
   }
 });
+
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err));
+}
+
+async function test() {
+  const user = await getUser<User>();
+
+  if (user) {
+    return user.result.name;
+  }
+}
+
+test().then(name => {
+  console.log(name);
+});
