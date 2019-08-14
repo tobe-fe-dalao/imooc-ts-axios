34219d32d6b8800b5008cb3ceed67815752f4334
yanxinchen1990 <yxc900920@163.com>
2019-08-14 17:26:20 +0800

    写几个注释

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -5,12 +5,17 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'development',
 
+  /**
+   * 遍历当前路径下所有文件夹，将文件夹中的 app.ts 文件作为入口文件
+   */
   entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
     const fullDir = path.join(__dirname, dir);
 
+    // 判断是否是文件夹
     if (fs.statSync(fullDir).isDirectory()) {
       const entry = path.join(fullDir, 'app.ts');
 
+      // 判断 app.ts 文件是否存在
       if (fs.existsSync(entry)) {
         entries[dir] = ['webpack-hot-middleware/client', entry];
       }
