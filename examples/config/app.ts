26ba9b9e7bb915237d38c6b877677816c918373a
yanxinchen1990 <yxc900920@163.com>
2019-08-14 17:22:49 +0800

    create方法example

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,20 +1,54 @@
 import axios, { AxiosTransformer } from '../../src/index';
 import qs from 'querystring';
 
-axios.defaults.headers.common['test2'] = 123;
+/**
+ * example-1
+ */
+// axios.defaults.headers.common['test2'] = 123;
+//
+// axios({
+//   method: 'post',
+//   url: '/config/post',
+//   data: qs.stringify({ a: 1 }),
+//   headers: {
+//     test: '321'
+//   }
+// }).then(response => {
+//   console.log(response.data);
+// });
 
-axios({
-  method: 'post',
-  url: '/config/post',
-  data: qs.stringify({ a: 1 }),
-  headers: {
-    test: '321'
-  }
-}).then(response => {
-  console.log(response.data);
-});
+/**
+ * example-2
+ */
+// axios({
+//   transformRequest: [
+//     (function (data) {
+//       return qs.stringify(data);
+//     }),
+//     ...(axios.defaults.transformRequest as AxiosTransformer[])
+//   ],
+//   transformResponse: [
+//     ...(axios.defaults.transformResponse as AxiosTransformer[]),
+//     function (data) {
+//       if (typeof data === 'object') {
+//         data.b = 2;
+//       }
+//       return data;
+//     }
+//   ],
+//   url: '/config/post',
+//   method: 'post',
+//   data: {
+//     a: 1
+//   }
+// }).then(response => {
+//   console.log(response.data);
+// });
 
-axios({
+/**
+ * example-3
+ */
+const instance = axios.create({
   transformRequest: [
     (function (data) {
       return qs.stringify(data);
@@ -30,6 +64,9 @@ axios({
       return data;
     }
   ],
+});
+
+instance({
   url: '/config/post',
   method: 'post',
   data: {
